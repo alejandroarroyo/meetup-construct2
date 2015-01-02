9e65ff2c63eb1a866d44eb08841f11d9ec84bea3
alejandroarroyo <alejandro_-90@hotmail.com>
2015-01-02 23:02:31 +0100

    fix(menu): eliminado

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,6 @@ window.onload = function() {
     game = new Phaser.Game(800, 600, Phaser.AUTO, 'slides-game');
     game.state.add('boot', ns.Boot);
     game.state.add('preloader', ns.Preloader);
-    game.state.add('menu', ns.Menu);
     game.state.add('game', ns.Game);
 
     game.state.start('boot');
